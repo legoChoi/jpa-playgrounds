e4ac4f61a8af20a73e4dcad6014f99fad89af5fd
legoChoi <chlx159@gmail.com>
2024-12-27 17:27:49 +0900

    Initial commit

---
+++ src/main/java/jpa/playgrounds/PlaygroundsApplication.java
@@ -0,0 +1,13 @@
+package jpa.playgrounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa/playgrounds/PlaygroundsApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.playgrounds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
