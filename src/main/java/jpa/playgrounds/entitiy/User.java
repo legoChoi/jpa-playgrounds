8d57cbc91d6d0823da43ff2091f502c6f7826155
legoChoi <chlx159@gmail.com>
2024-12-27 17:44:53 +0900

    user entity

---
+++ src/main/java/jpa/playgrounds/entitiy/User.java
@@ -0,0 +1,17 @@
+package jpa.playgrounds.entitiy;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10, nullable = false)
+    private String userName;
+}
